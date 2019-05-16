1da6b6813edaad7f37b97223b1271d72ff5e0db8
J.Rafael Ramón Navarro <rramon@plainconcepts.com>
2019-05-16 11:49:27 +0200

    Dependency Injection

---
+++ src/Functions/Configuration/Config.cs
@@ -0,0 +1,20 @@
+﻿using Functions.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+[assembly: FunctionsStartup(typeof(Functions.Configuration.Config))]
+namespace Functions.Configuration
+{
+    public class Config : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var services = builder.Services;
+
+            services.AddSingleton<IConfiguration>();
+            services.AddScoped<IWoffuToken, WoffuToken>();
+        }
+    }
+}
