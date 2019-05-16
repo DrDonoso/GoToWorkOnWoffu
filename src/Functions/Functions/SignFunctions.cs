40dda091b447522a4f63ef3dc25689a041ee244e
Marçal <marcal.montserrat@gmail.com>
2019-05-16 10:04:38 +0200

    added dayService to check if today is holiday (#2)

--- src/Functions/Functions/SignFunctions.cs
+++ src/Functions/Functions/SignFunctions.cs
@@ -1,5 +1,8 @@
+using Functions.Service;
 using Functions.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
@@ -32,6 +35,23 @@ namespace Functions.Functions
             var userId = _configuration["UserId"];
 
             await SignService.Sign(authKey, Convert.ToInt32(userId));
+        }
+
+        [FunctionName("IsHolidayPost")]
+        public async Task IsHolidayPost([HttpTrigger(AuthorizationLevel.Function, "get")]HttpRequest req, ILogger log, ExecutionContext context)
+        {
+
+            //var _configuration = new ConfigurationBuilder()
+            //    .SetBasePath(context.FunctionAppDirectory)
+            //    .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            //    .AddEnvironmentVariables()
+            //    .Build();
+
+            var authKey = _configuration["AuthToken"];
+            var userId = _configuration["UserId"];
+
+            var response = await DayService.IsHoliday(authKey, Convert.ToInt32(userId));
+            Console.WriteLine(response);
         }
     }
 }
